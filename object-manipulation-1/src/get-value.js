d4e82eb4b79eb29076d602ed2abb067e93612fd9
Calvin Tran <calvinxtran@gmail.com>
2022-05-19 23:32:00 +0000

    completed object-manipulation-1

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,9 @@
+/* exported getValue */
+
+// assign key of object to a variable --complated
+// return variable --completed
+
+function getValue(object, key) {
+  var value = object[key];
+  return value;
+}
---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,11 @@
+/* exported setValue */
+
+// use bracket notation to push key into object --completed
+// assign the value to the pushed key --completed
+// return that expression
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
+
+// function does not contain return statement
