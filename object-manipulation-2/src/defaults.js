53d437c1731c3cc6cc3db4f748045bbcd0c25d98
Calvin Tran <calvinxtran@gmail.com>
2022-05-21 06:02:58 +0000

    completed object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,16 +1,9 @@
 /* exported defaults */
 
-// function defaults(target, source) {
-//   for (var property in source) {
-//     if (source.hasOwnProperty(property) && !target.hasOwnProperty(property)) {
-//       target[property] = source[property];
-//     }
-//   }
-// }
-
-function defaults(mainObject, objectToMerge) {
-  Object.keys(objectToMerge).forEach(key => {
-    if (!mainObject[key]) mainObject[key] = objectToMerge[key];
-  });
-  return mainObject;
+function defaults(target, source) {
+  for (var property in source) {
+    if (property in target === false) {
+      target[property] = source[property];
+    }
+  }
 }
