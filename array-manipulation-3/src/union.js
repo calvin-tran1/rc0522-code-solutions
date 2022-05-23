f1ad3c671a4f0520ac44cebd30001b01c8307fa4
Calvin Tran <calvinxtran@gmail.com>
2022-05-23 18:19:26 +0000

    switching workspace

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,20 @@
 /* exported union */
+
+function union(first, second) {
+  var result = [];
+
+  for (var i = 0; i < first.length; i++) {
+    var found = false;
+
+    for (var j = 0; j < second.length; j++) {
+      if (first[i] === second[j]) {
+        found = true;
+        break;
+      }
+    }
+    if (found === false) {
+      result.push(first[i], second[i]);
+    }
+  }
+  return result;
+}
