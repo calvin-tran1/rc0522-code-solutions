5a469e37d2cb4a506009edd08dac3756ae8cc80e
Calvin Tran <calvinxtran@gmail.com>
2022-05-27 19:04:49 +0000

    completed javascript-local-storage

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,11 @@
+/* exported todos */
+
+var todos = [];
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+
+todos = JSON.parse(previousTodosJSON);
+
+window.addEventListener('beforeunload', event => {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+});
