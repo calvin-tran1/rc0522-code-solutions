c01af8c8a3c880ca75477518b22f64d60fb52deb
Calvin Tran <calvinxtran@gmail.com>
2022-05-27 19:09:56 +0000

    added conditional statement

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -3,7 +3,9 @@
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 
-todos = JSON.parse(previousTodosJSON);
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
 
 window.addEventListener('beforeunload', event => {
   var todosJSON = JSON.stringify(todos);
