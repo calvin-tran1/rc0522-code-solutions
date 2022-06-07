cc51e8c61dabe37217d7ee2433d1e681ae750a9d
Calvin Tran <calvinxtran@gmail.com>
2022-06-07 22:40:32 +0000

    completed javascript-bank

---
+++ javascript-bank/bank.js
@@ -0,0 +1,37 @@
+/* exported Bank */
+
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (Number.isInteger(balance) && balance > 0) {
+    var account = new Account(this.nextAccountNumber, holder);
+    account.deposit(balance);
+    this.accounts.push(account);
+    this.nextAccountNumber++;
+    return account.number;
+  }
+  return null;
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var total = 0;
+
+  if (this.nextAccountNumber > 1) {
+    for (var i = 0; i < this.accounts.length; i++) {
+      total += this.accounts[i].getBalance();
+    }
+  }
+  return total;
+};
