1a6f63eeb028757c51b2364e7fa014d60b4ee238
Calvin Tran <calvinxtran@gmail.com>
2022-06-08 17:36:29 +0000

    updated javascript-timers to clear original interval

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,13 +1,12 @@
 var $h1 = document.querySelector('h1');
 var counter = 4;
+var countdownTimer = setInterval(countdown, 1000);
 
 function countdown() {
   if (counter > 0) {
     $h1.textContent = counter--;
   } else if (counter === 0) {
     $h1.textContent = '~Earth Beeeelooowww Us~';
-    clearInterval(setInterval(countdown, 1000));
+    clearInterval(countdownTimer);
   }
 }
-
-setInterval(countdown, 1000);
