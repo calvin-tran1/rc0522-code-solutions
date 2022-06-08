3ea8140e4948a686b977eb45da0fc8ea33503b39
Calvin Tran <calvinxtran@gmail.com>
2022-06-08 18:02:10 +0000

    counter now stops at 1

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -3,10 +3,10 @@ var counter = 4;
 var countdownTimer = setInterval(countdown, 1000);
 
 function countdown() {
-  if (counter > 0) {
+  if (counter > 1) {
     counter -= 1;
     $h1.textContent = counter;
-  } else if (counter === 0) {
+  } else if (counter === 1) {
     $h1.textContent = '~Earth Beeeelooowww Us~';
     clearInterval(countdownTimer);
   }
