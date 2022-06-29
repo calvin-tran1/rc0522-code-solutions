b7d772d633708567d29346ddf7e33ea89dbb9933
Calvin Tran <calvinxtran@gmail.com>
2022-06-29 00:29:48 +0000

    completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,5 @@
+console.log('exports is a typeof:', typeof exports);
+console.log('require is a typeof:', typeof require);
+console.log('module is a typeof:', typeof module);
+console.log('__filename is a typeof:', typeof __filename);
+console.log('__dirname is a typeof:', typeof __dirname);
