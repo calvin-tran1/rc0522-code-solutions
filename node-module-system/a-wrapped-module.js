8c5e9628c33bfadea18c8a483bef7be34731ea8a
Calvin Tran <calvinxtran@gmail.com>
2022-06-29 17:07:08 +0000

    updated node-module-system

--- node-module-system/a-wrapped-module.js
+++ node-module-system/a-wrapped-module.js
@@ -1,5 +1,14 @@
+console.log('value of exports:', exports);
 console.log('exports is a typeof:', typeof exports);
+
+console.log('value of require:', require);
 console.log('require is a typeof:', typeof require);
+
+console.log('value of module:', module);
 console.log('module is a typeof:', typeof module);
+
+console.log('value of __filename:', __filename);
 console.log('__filename is a typeof:', typeof __filename);
+
+console.log('value of __dirname:', __dirname);
 console.log('__dirname is a typeof:', typeof __dirname);
