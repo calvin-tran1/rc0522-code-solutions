6eea4a33e35a6de7d49bbe2d9743deac2bc32300
Calvin Tran <calvinxtran@gmail.com>
2022-07-17 07:33:05 +0000

    still in progress

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
 
 export default class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { lang: '' };
+  }
 
+  handleClick(e) {
+    this.setState({ lang: e.target.getAttribute('name') });
+  }
+
+  render() {
+
+    return (
+      <ul>
+        {this.props.languages.map(lang => {
+          return (
+          <li key={lang.shorthand}>
+            <div name={lang.shorthand} className="lang" onClick={this.handleClick}>
+              {lang.language}
+            </div>
+            <LanguageDescription description={lang.description} />
+          </li>
+          );
+        })
+      }
+      </ul>
+    );
+  }
+}
+
+function LanguageDescription(props) {
+  return (
+    <div name={props.shorthand} className="lang-description">
+      {props.description}
+    </div>
+  );
 }
